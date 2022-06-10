d5243f2e4f0f83a1285f4bbccdeef7674cb76ac5
ngthanhnhan0812 <ntnhan0812a@gmail.com>
2022-06-10 20:39:51 +0700

    master

---
+++ app/Http/Controllers/adminController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+class adminController extends Controller
+{
+
+}
